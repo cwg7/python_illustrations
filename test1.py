f2553e6edf090d8aa3767e5b37addab29ffa28fd
cwins <cwinston7g@gmail.com>
2022-02-18 12:30:06 -0500

    lambda example

--- test1.py
+++ test1.py
@@ -1,1 +1,14 @@
-print("This is a test")
+ # print("This is a test")
+
+# lambda example:
+
+seq = ['soup','dog','salad','cat','great']
+
+# filter this list such that it only contains words that begin with the letter 's':
+
+
+filteredList = list(filter(lambda word: word[0]=='s',seq))
+
+print(filteredList)
+
+

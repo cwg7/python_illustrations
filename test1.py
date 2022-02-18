bfbe85239c427ac08d4f878e38beafbcfce48233
cwins <cwinston7g@gmail.com>
2022-02-18 12:48:23 -0500

    function example

--- test1.py
+++ test1.py
@@ -12,3 +12,21 @@ filteredList = list(filter(lambda word: word[0]=='s',seq))
 print(filteredList)
 
 
+# *You are driving a little too fast, and a police officer stops you. Write a function to return one of 3 possible results:
+ # "No ticket", "Small ticket", or "Big Ticket". If your speed is 60 or less, the result is "No Ticket".
+ # If speed is between 61 and 80 inclusive, the result is "Small Ticket". If speed is 81 or more, the result is "Big Ticket".
+ # Unless it is your birthday (encoded as a boolean value in the parameters of the function) -- on your birthday, your speed can be 5 higher in all cases. *
+
+def speeding(speed, birthday):
+
+    if birthday == True:
+        speed -= 5
+    elif speed <= 60:
+         print("No Ticket")
+    elif speed >= 61 & speed <= 80:
+         print("Small Ticket")
+    elif speed >= 81:
+         print("Big Ticket")
+
+#print(speeding(61, True))
+

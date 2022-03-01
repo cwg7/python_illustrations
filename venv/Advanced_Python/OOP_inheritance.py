b2f93017ae6deea88842216f124e1752e365706a
cwins <cwinston7g@gmail.com>
2022-03-01 15:17:50 -0500

    OOP inheritance

---
+++ venv/Advanced_Python/OOP_inheritance.py
@@ -0,0 +1,87 @@
+class Person():
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def hello(self):
+        print("hello")
+
+    def report(self):
+        print(f"I am {self.first_name} {self.last_name}")
+
+
+# x = Person('John', "Smith")
+#
+# x.hello()
+# x.report()
+
+# Agent class inherits methods with Person class
+
+class Agent(Person):
+
+    def __init__(self, first_name, last_name, code_name):
+        Person.__init__(self, first_name, last_name)
+        self.code_name = code_name
+
+    # Overriding report method here
+    def report(self):
+        print("I am here.")
+
+    def reveal(self, passcode):
+        if passcode == 123:
+            print("I am a secret agent")
+        else:
+            self.report()
+
+
+x = Agent('John', 'Smith', 'mr.X')
+x.hello()
+# x.report()
+# x.reveal(12)
+x.reveal(123)
+
+print(x.code_name)
+print("\n\n\n")
+
+
+# Exercise
+# Create a class Animal which accepts a name and species. IT should have a function called
+# greet() which prints the statemnt: "Hi! My name is {name} and I am a {species}
+
+
+class Animal():
+
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def greet(self):
+        print(f"Hi! My name is {self.name} and I am a {self.species}")
+
+
+    # Then use this class to create the classes Cat and Dog. Each class should accept a name
+    # and implement a method called sound() which prints the sound the animal typically makes
+
+
+class Cat(Animal):
+
+    def __init__(self, name):
+        self.name = name
+
+    def sound(self):
+        print("Meow")
+
+
+class Dog(Animal):
+    def __init__(self,name):
+        self.name = name
+    def sound(self):
+        print("RufF")
+
+
+cat1 = Cat('snickers')
+cat1.sound()
+
+dog1 = Dog('Buzz')
+dog1.sound()

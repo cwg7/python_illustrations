66abb4745d32bf190911c38c3f6f573cf2b9b421
cwins <cwinston7g@gmail.com>
2022-03-01 14:29:40 -0500

    OOP example

--- venv/Advanced_Python/pythonOOP.py
+++ venv/Advanced_Python/pythonOOP.py
@@ -27,10 +27,64 @@
 
 class Sample():
 
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
 
 
-x = Sample(name = "Chase")
-print(x.name)
+x = Sample(name="Chase")
 
+
+# print(x.name)
+
+
+class Student():
+    # These are Class Object Attributes
+    species = "Humans"
+    planet = "Earth"
+
+    ##
+
+    def __init__(self, name, gpa):
+        self.name = name
+        self.gpa = gpa
+
+
+student1 = Student(name="Chase", gpa=4.0)
+student2 = Student(name="Rando", gpa=3.0)
+
+print(student2.gpa)
+print(student1.planet)
+
+
+# Create another class
+
+class Agent():
+    origin = "USA"
+
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+
+x = Agent('Agent0', 6, 200)
+x.weight = 210
+print(x.weight)
+
+
+# Exercise
+# Create a class Dog which accepts the dog's name and breed
+# Use this class to create 2 dogs: "German Shepherd: Hans" & "Labrador: Lou"
+# Finally use an f-string to print the name of the 2 dogs
+# ie 'Hans and Lou are friends'
+
+class Dog():
+
+    def __init__(self,breed,name):
+        self.breed = breed
+        self.name = name
+
+dog1 = Dog("German Shepherd", "Hans")
+dog2 = Dog("Labrador", "Lou")
+
+print(f"{dog1.name} and {dog2.name} are friends")

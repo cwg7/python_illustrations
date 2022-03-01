a4becedce8b6716bed013ceb3286b72f6a1d099b
cwins <cwinston7g@gmail.com>
2022-03-01 13:01:16 -0500

    exception_handling

--- venv/Advanced_Python/exceptionHandling.py
+++ venv/Advanced_Python/exceptionHandling.py
@@ -1,2 +1,24 @@
 #errors and exception handling
 
+# cannot 'catch' syntax error
+#print("hello)
+
+# example of catching a type error
+try:
+    print('10' + 10)
+
+except IOError:
+    print("You have an input/output error")
+    print("Did you check the file permissions?")
+except TypeError:
+    print("You are using the wrong data types")
+
+except:
+    print("Hey you got an error")
+
+finally:
+    print("FINALLY WILL ALWAYS RUN, ERROR OR NO ERROR")
+
+# adding multiple acceptions in a try/catch allows you to better determine
+# cause of error(s)
+
